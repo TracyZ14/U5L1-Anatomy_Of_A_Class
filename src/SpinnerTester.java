c69cf3eef3439cf0f797c69f325d04a9c6c12dab
BT_4N2_07 <TracyZ14@nycstudents.net>
2023-11-30 13:14:46 -0500

    Finished the SpinnerTester class and tested the Spinner class

--- src/SpinnerTester.java
+++ src/SpinnerTester.java
@@ -4,12 +4,15 @@ public class SpinnerTester
     {
         Spinner a = new Spinner(5);
 
-        for(int i = 0; i < 10; i++);
+        for(int i = 0; i < 10; i++)
         {
             a.spin();
 
-            System.out.println("EXPECTED RESULT : [random number between 1 - 5]");
+            System.out.println("EXPECTED RESULT : [random number between 1 - 5, inclusive]");
             System.out.println("ACTUAL RESULT   : " + a.getMostRecentSpinValue());
+
+            System.out.println("EXPECTED RESULT : [the average of all the previous random numbers]");
+            System.out.println("ACTUAL RESULT   : " + a.getAverageSpinValue());
         }
     }
 }
